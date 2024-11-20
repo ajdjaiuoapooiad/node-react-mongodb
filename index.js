f3e345fd87247ee0b95e98ee4e9e4a30f88ed125
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-20 14:23:04 +0900

    oj

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => console.log('Database is connected')).catch((err) => console.log(err));
+
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    isMarried: Boolean,
+    salary: Number,
+    gender: String,
+
+})
+
+const User = mongoose.model('User',userSchema);
+
+    async function storeInfomation(){
+        const user = new User({
+            name: "yukito",
+            age: 23,
+            isMarried: false,
+            salary: 300,
+            gender: "Male",
+        });
+        await user.save();
+        console.log(user);
+        
+    }
+
+
